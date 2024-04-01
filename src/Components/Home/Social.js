76067f740b3a69f320e47f9e5dd3eda21cb509ac
ArbiyaSultana <alra8006@gmail.com>
2024-04-01 16:25:17 +0530

    second

--- src/Components/Home/Social.js
+++ src/Components/Home/Social.js
@@ -3,7 +3,7 @@ import { FaTwitter,FaInstagram,FaLinkedin,FaGithub } from 'react-icons/fa'
 import './Home.css'
 function Social() {
   return (
-    <div className='icons'>
+    <div className='home-icons'>
       <a href="https://wwww.instagram.com" className='insta' target="_blank"><FaInstagram size={30} style={{color:"#FFF"}}/></a>
       <a href="https://github.com/sultanaarbiya" className='github' target="_blank"><FaGithub size={30} style={{color:"#FFF"}}/></a>
       <a href="https://in.linkedin.com/" className='linkedin' target="_blank"><FaLinkedin size={30} style={{color:"#FFF"}}/></a>
--- src/Components/MyPortfolio/Slider.js
+++ src/Components/MyPortfolio/Slider.js
@@ -1,41 +1,44 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Pagination } from 'swiper/modules';
+
 // Import Swiper styles
 import 'swiper/css';
-// import 'swiper/css/grid';
-// import 'swiper/css/pagination';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
 
-import './MyPortfolio';
+import './MyPortfolio.css'
 
 // import required modules
-// import { Grid } from 'swiper/modules';
+import { Grid, Pagination } from 'swiper/modules';
 
 export default function App() {
   return (
     <>
       <Swiper
-
-      spaceBetween={50}// space between slides
-        slidesPerView={3}
+        slidesPerView={2}
         grid={{
           rows: 2,
+          fill:"row",
         }}
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        // modules={[Grid, Pagination]}
+        modules={[Grid, Pagination]}
         className="mySwiper"
+      >
        
->
-     <SwiperSlide ><img /></SwiperSlide>
-     <SwiperSlide ><img /></SwiperSlide>
-     <SwiperSlide ><img /></SwiperSlide>
-     <SwiperSlide ><img /></SwiperSlide>
-     <SwiperSlide><img /></SwiperSlide>
-  
+        <SwiperSlide className='swipe'> <img src={require("./Images/image1.png")} style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/image2.png")} style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/image3.png")} style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/imag4.png")} style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/imag5.png")} style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/image6.png")}  style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/image7.png")}  style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/image8.png")}  style={{width:"100%"}}></img></SwiperSlide>
+        <SwiperSlide className='swipe'> <img src={require("./Images/tindog.jpg")}  style={{width:"100%"}}></img></SwiperSlide>
+       
       </Swiper>
     </>
   );
